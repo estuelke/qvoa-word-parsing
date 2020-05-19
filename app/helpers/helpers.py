fe872ec57f20c2074b9f6ae7c932824a39519631
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-05-19 11:17:32 -0400

    removed unused functions

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -1,5 +1,3 @@
-import pandas as pd
-from app.helpers import names
 from openpyxl import load_workbook
 from openpyxl.worksheet.table import Table, TableStyleInfo
 
@@ -26,20 +24,3 @@ def add_tables(filename):
         print(f"{sheet} table generated")
 
     wb.save(filename)
-
-
-def create_dataframe(name, data):
-    if name == names.PH_INFO:
-        return {names.RAW: pd.DataFrame(data).set_index(names.PH)}
-    else:
-        return {names.RAW: pd.DataFrame(data)}
-
-
-def merge_dicts(dicts):
-    return {k: d[k] for d in dicts for k, v in d.items()}
-
-
-def extend_data(data, cumulative_data):
-    for name, items in data.items():
-        cumulative_data[name].extend(items)
-    return cumulative_data
