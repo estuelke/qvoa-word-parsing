931dd3be4c32f604c7ad82eb64bbb94aefab3dea
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-06-11 16:51:51 -0400

    removed commented code

--- app/patterns/days.py
+++ app/patterns/days.py
@@ -26,27 +26,6 @@ DAY_WITH_EXP_NOTE = rf"""^
     (?P<{names.EXPERIMENT_NOTE}>one\smore.*?added\sat\sday\s\d+)
 $"""
 
-# DAY_WITH_INFO = (
-#     rf"""^
-#         \#
-#         (?P<{names.PH_VERIFY}>\d+)\s # PH Number
-#         (?P<{names.PH_DATE}>Nov\s05)\s # PH Date
-#         (?P<{names.PID}>TC5006)\sSubject\s4\s # Patient ID (multiple)
-#         (?P<{names.TIMEPOINT}>Week\s0b)\s # Study Timepoint
-#         (?P<{names.STUDY_NAME}>Erad\sstudy)\s # Study Name
-#         Day\s(?P<{names.DAY}>\d+)\sin\sculture
-#     $""",
-#     rf"""^
-#         \#
-#         (?P<{names.PH_VERIFY}>\d+)\s # PH Number
-#         (?P<{names.PH_DATE}>Nov\s05)\s # PH Date
-#         TC5006\s(?P<{names.PID}>Subject\s4)\s # Patient ID (multiple)
-#         (?P<{names.TIMEPOINT}>Week\s0b)\s # Study Timepoint
-#         (?P<{names.STUDY_NAME}>Erad\sstudy)\s # Study Name
-#         Day\s(?P<{names.DAY}>\d+)\sin\sculture
-#     $"""
-# )
-
 DAY_WITH_INFO = rf"""^
     \#
     (?P<{names.PH_VERIFY}>\d+)\s # PH Number
